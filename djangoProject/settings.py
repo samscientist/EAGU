ac55a907d6fb78a3e2af37306de36c6b04951f65
admin <admin@admin.com>
2022-06-28 14:13:49 +0900

    (re-)Host Allowing

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
